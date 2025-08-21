addae7f893f94f530d17f1e4c41b57730a85be57
muntasir17 <166358439+muntasir17@users.noreply.github.com>
2025-08-21 09:37:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let synonymText = document.querySelector(".synonymText");
 let antonymText = document.querySelector(".antonymText");
 let wordInput = document.querySelector(".wordInput");
 let btn = document.querySelector(".btn");
-
 btn.addEventListener("click",()=>{
 let input = wordInput.value;
 if(input === ""){
@@ -41,19 +40,3 @@ console.log(data.antonyms);
 });
 }
 });
-
-// let url = 'https://api.api-ninjas.com/v1/thesaurus?word=' + text
-// fetch(url,{
-// headers: {
-// 'X-Api-Key': 'FxIIfne1F11VDaFrvODTcg==t7fkU4TSrkUw5VVA'
-// }
-// })
-// .then(response => response.json())
-// .then(data => {
-// console.log(data.synonyms);
-// console.log(data.antonyms);
-// let rando1 = data.synonyms[Math.floor(Math.random() * data.synonyms.length)];
-// console.log(rando1);
-// let rando2 = data.antonyms[Math.floor(Math.random() * data.antonyms.length)];
-// console.log(rando2);
-// });
